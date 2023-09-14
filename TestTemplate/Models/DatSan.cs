59d32a799652940a23b41e32129b3a99ce01f2a0
DANG MINH <minhdangngoc304@gmail.com>
2023-09-14 22:02:20 +0700

    Tạo form đặt sân

---
+++ TestTemplate/Models/DatSan.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestTemplate.Models
+{
+    public class DatSan
+    {
+        public string hoTen { get; set; }
+        public string email { get; set; }
+        public string soDienThoai { get; set; }
+        public string soSan { get; set; }
+        public DateTime gioBatDau { get; set; }
+        public DateTime gioKetThuc { get; set; }
+    }
+}
