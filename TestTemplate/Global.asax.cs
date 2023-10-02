1030cb7e848268b351dc095b855a438aeffe8329
DANG MINH <minhdangngoc304@gmail.com>
2023-10-02 14:40:59 +0700

    thay đổi vùng <globalization culture = "en-GB"> trong Web.config

--- TestTemplate/Global.asax.cs
+++ TestTemplate/Global.asax.cs
@@ -5,6 +5,7 @@ using System.Web;
 using System.Web.Mvc;
 using System.Web.Optimization;
 using System.Web.Routing;
+using System.Threading;
 
 namespace TestTemplate
 {
@@ -16,6 +17,8 @@ namespace TestTemplate
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+            Thread.CurrentThread.CurrentCulture = System.Globalization.CultureInfo.InvariantCulture;
+
         }
     }
 }
