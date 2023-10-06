d941076151329312b142eb17f5498c5383699567
DANG MINH <minhdangngoc304@gmail.com>
2023-10-06 14:57:05 +0700

    load dữ liệu từ CSDL lên trang bóng rổ và tennis

--- TestTemplate/Controllers/BasketballController.cs
+++ TestTemplate/Controllers/BasketballController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestTemplate.Models;
 
 namespace TestTemplate.Controllers
 {
     public class BasketballController : Controller
     {
         // GET: Basketball
+        QuanLyDatSanEntities db = new QuanLyDatSanEntities();
         public ActionResult Index()
         {
-            return View();
+            var lstCsBongRo = db.cosoes.Where(n => n.mals == "br");
+            return View(lstCsBongRo);
         }
     }
 }
--- TestTemplate/Controllers/TennisController.cs
+++ TestTemplate/Controllers/TennisController.cs
@@ -3,15 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using TestTemplate.Models;
 
 namespace TestTemplate.Controllers
 {
     public class TennisController : Controller
     {
         // GET: Tennis
+        QuanLyDatSanEntities db = new QuanLyDatSanEntities();
         public ActionResult Index()
         {
-            return View();
+            var lstCsTennis = db.cosoes.Where(n => n.mals == "tennis");
+            return View(lstCsTennis);
         }
     }
 }
